29a1507388fdc6f8ea755c90cc72cdf7c57a9941
Angel Popov <apopov@distributor>
2014-10-19 18:35:27 +0300

    refactored

--- logged.lisp
+++ logged.lisp
@@ -1,37 +1,23 @@
-(defun read-temps()
-  (loop for sensor in '("outdoor" "garage" "kitchen" "living-room" "office" "kids")
-       for last in prev
-     :collect
-       (let ((res (handler-case
-		      (with-open-file (f (string-concat "temperatures/" sensor) :direction :input)
-			(read f nil nil))
-		    (error (e) (format nil "Error ~E" e) nil))))
-	 (cond
-	   ((null res) last)
-	   ((> (abs (- res last))1) (+ last (/ (abs(- res last)) (- res last)))) 
-	   (t res)))))
-
-(defun get-info( prev)
-  (loop for sensor in '("outdoor" "garage" "kitchen" "living-room" "office" "kids")
-       for last in prev
-     :collect
-       (let ((res (handler-case
-		      (with-open-file (f (string-concat "temperatures/" sensor) :direction :input)
-			(read f nil nil))
-		    (error (e) (format nil "Error ~E" e) nil))))
-	 (cond
-	   ((null res) last)
-	   ((> (abs (- res last))1) (+ last (/ (abs(- res last)) (- res last)))) 
-	   (t res)))))
+(defvar *sensors* '("outdoor" "garage" "kitchen" "living-room" "office" "kids"))
+(defun read-sensor(sensor)
+  (handler-case
+      (with-open-file (f (string-concat "~/temperatures/" sensor) :direction :input)
+	(read f nil nil))
+    (error (e) (format nil "Error ~E" e) nil)))
+(defun filter-latest(last prev)
+  (cond
+    ((null last) prev)
+    ((and prev (> (abs (- prev last))1)) (+ last (/ (abs(- prev last)) (- prev last))))
+    (t last)))
 
 (defun save-current(current)
-  (delete-file "prev.lisp")
-  (with-open-file (f "prev.lisp" :direction :output)
+  (delete-file "~/prev.lisp")
+  (with-open-file (f "~/prev.lisp" :direction :output)
       (format f "(defparameter *prev* '~S)" current)))
 
 (defun main()
-  (load "prev.lisp")
-  (let ((res (get-info *prev*)))
-    (save-current res)
+  (load "~/prev.lisp")
+  (let ((res (mapcar #'filter-latest (mapcar #'read-sensor *sensors*) *prev*)))
+    ;(save-current res)
     (format t "~{~F ~}~%" res)))
 (main)
